a0dacc090c7ea78972b60a21e5810e459a354288
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-03-02 17:58:27 +0100

    finish phonebook

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,3 +1,16 @@
-export default function SearchBox() {
-    return ();
+import clsx from "clsx";
+import s from "./SearchBox.module.css";
+
+export default function SearchBox({ value, onFilter }) {
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        className={clsx(s.srchInpt)}
+        value={value}
+        onChange={(e) => onFilter(e.target.value)}
+      />
+    </div>
+  );
 }
